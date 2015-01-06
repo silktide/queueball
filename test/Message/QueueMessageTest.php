9c356f8deb34af9f476e92779f1277d06c489323
Danny Smart <dannysmart@silktide.com>
2015-01-06 13:20:06 +0000

    QueueMessage test

---
+++ test/Message/QueueMessageTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Silktide Nibbler. Copyright 2013-2014 Silktide Ltd. All Rights Reserved.
+ */
+namespace Silktide\QueueBall\Test\Message;
+use Silktide\QueueBall\Message\QueueMessage;
+
+/**
+ *
+ */
+class QueueMessageTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @dataProvider properties
+     *
+     * @param string $property
+     * @param string|null $type
+     */
+    public function testProperty($property, $type = null)
+    {
+        $message = new QueueMessage();
+        switch ($type) {
+            case "int":
+                $value = 3;
+                break;
+            case "float":
+                $value = 1.24;
+                break;
+            case "string":
+                $value = "test";
+                break;
+            case "bool":
+                $value = true;
+                break;
+            case "array":
+                $value = [1, 2, 3];
+                break;
+            case null;
+            case "null":
+                $value = null;
+                break;
+            default:
+                $value = \Mockery::mock($type);
+                break;
+        }
+        $message->{"set" . $property}($value);
+        $this->assertSame($value, $message->{"get" . $property}(), "The property '" . lcfirst($property). "' did not return the expected value");
+    }
+
+    /**
+     * @return array
+     */
+    public function properties()
+    {
+        return [
+            ["Id"],
+            ["Message"],
+            ["ReceiptId"],
+            ["Attributes", "array"],
+            ["QueueId"]
+        ];
+    }
+
+}
+ 
